af9191adfe18f52e33f9313609c1fe424ffe8d34
boymeetsblockchain <ajeoluwasegun18@gmail.com>
2023-01-23 17:27:53 -0800

    fixed the bug

---
+++ backend/routes/taskRoute.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const Task = require('../model/taskModel')
+
+const router = express.Router()
+
+// create a Task
+router.post("/api/tasks",async (req,res)=>{
+    try {
+      const task = await Task.create(req.body) 
+      res.status(200).json(task)
+    } catch (error) {
+      res.status(500).json({msg: error.message})
+    }
+   })
+
+   
+// get/Read Task
+  router.get("/api/tasks", async (req,res)=>{
+    try {
+     const tasks = await Task.find()
+     res.status(200).json(tasks)
+    } catch (error) {
+    res.status(500).json({msg: error.message}) 
+    }
+ })
+module.exports = router
